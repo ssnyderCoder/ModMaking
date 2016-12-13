b55330cc80c131493ec88768b7f02a3fc746d60e
ssnyderCoder <ogresean@yahoo.com>
2016-12-13 10:11:31 -0600

    Refined BuildTheme

--- src/main/java/com/seanModTest/templates/BuildBlockData.java
+++ src/main/java/com/seanModTest/templates/BuildBlockData.java
@@ -2,28 +2,62 @@ package com.seanModTest.templates;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.command.NumberInvalidException;
+import net.minecraft.util.ResourceLocation;
 
 public class BuildBlockData {
 
+	public static final String DEFAULT_BLOCK_NAME = "minecraft:stone";
+	public static final int DEFAULT_META = 0;
+	public static final String[] DEFAULT_TILE_DATA = {""};
+	
 	//perhaps change to use specific data tags instead of meta
 	private final String blockName;
 	private final int meta;
+	private final String[] tileEntityData;
+	public BuildBlockData(String uniqueBlockName, int metaValue, String[] tileData) {
+		blockName = uniqueBlockName;
+		meta = metaValue;
+		tileEntityData = tileData;
+	}
+	
 	public BuildBlockData(String uniqueBlockName, int metaValue) {
 		blockName = uniqueBlockName;
 		meta = metaValue;
+		tileEntityData = DEFAULT_TILE_DATA;
 	}
+	
+	public BuildBlockData(String uniqueBlockName) {
+		blockName = uniqueBlockName;
+		meta = DEFAULT_META;
+		tileEntityData = DEFAULT_TILE_DATA;
+	}
+	
+	public BuildBlockData() {
+		blockName = DEFAULT_BLOCK_NAME;
+		meta = DEFAULT_META;
+		tileEntityData = DEFAULT_TILE_DATA;
+	}
+	
 	public String getBlockName() {
 		return blockName;
 	}
+	
 	public int getMetaValue() {
 		return meta;
 	}
+	
+	public String[] getUnprocessedTileEntityData(){
+		return tileEntityData;
+	}
+	
 	public IBlockState getBlockState(){
 		Block block = Block.getBlockFromName(blockName);
 		if(block != null){
-			block.getStateFromMeta(meta);
+			return block.getStateFromMeta(meta);
 		}
-		return null;
+		else return null;
 	}
+	
 
 }
--- src/main/java/com/seanModTest/templates/BuildTheme.java
+++ src/main/java/com/seanModTest/templates/BuildTheme.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import net.minecraft.init.Blocks;
 
@@ -18,15 +19,55 @@ public class BuildTheme {
 	private static final String SLAB = "slab";
 	private static final String FENCE = "fence";
 	private static final String DOOR = "door";
+	private static final String OTHER = "other";
 
+	//can get damaged variant of any block by prefacing new type with "d_"
+	//chance of block being damaged determined by BuildTheme
+	
 	private static final BuildTheme DEFAULT_BUILD_THEME = new BuildTheme();
 	{
-		
+		DEFAULT_BUILD_THEME.addBBData(PRIMARY, "minecraft:stonebrick");
+		DEFAULT_BUILD_THEME.addBBData(SECONDARY, "minecraft:cobblestone");
+		DEFAULT_BUILD_THEME.addBBData(DAMAGED_PRIMARY, new BuildBlockData("minecraft:stonebrick", 2)); //cracked
+		DEFAULT_BUILD_THEME.addBBData(DAMAGED_SECONDARY, "minecraft:mossy_cobblestone");
+		DEFAULT_BUILD_THEME.addBBData(WINDOW, "minecraft:glass");
+		DEFAULT_BUILD_THEME.addBBData(FURNITURE, "minecraft:jukebox");
+		DEFAULT_BUILD_THEME.addBBData(STAIRS, "minecraft:stone_brick_stairs");
+		DEFAULT_BUILD_THEME.addBBData(SLAB, "minecraft:stone_slab");
+		DEFAULT_BUILD_THEME.addBBData(FENCE, "minecraft:cobblestone_wall");
+		DEFAULT_BUILD_THEME.addBBData(DOOR, "minecraft:wooden_door");
+		DEFAULT_BUILD_THEME.addBBData(OTHER, "minecraft:dirt");
 	}
 	//use block names to designate blocks
-	private final Map<String, ArrayList<String>> typeToBlockData= new HashMap<String, ArrayList<String>>();
+	private final Map<String, ArrayList<BuildBlockData>> typeToBBData= new HashMap<String, ArrayList<BuildBlockData>>();
+	
 	public BuildTheme() {
-
 	}
-
+	
+	public boolean addBBData(String type, BuildBlockData bbd){
+		ArrayList<BuildBlockData> bbdList = typeToBBData.get(type);
+		if(bbdList == null){
+			bbdList = new ArrayList<BuildBlockData>();
+			typeToBBData.put(type, bbdList);
+		}
+		bbdList.add(bbd);
+		return true;
+	}
+	
+	protected Map<String, ArrayList<BuildBlockData>> getTypeToBBData(){
+		return typeToBBData;
+	}
+	
+	public boolean addBBData(String type, String blockName){
+		return addBBData(type, new BuildBlockData(blockName));
+	}
+	
+	public BuildBlockData getRandomBBData(Random rand, String type){
+		ArrayList<BuildBlockData> bbdList = typeToBBData.get(type);
+		if(bbdList == null){
+			bbdList = DEFAULT_BUILD_THEME.getTypeToBBData().get(type);
+			if(bbdList == null) bbdList = DEFAULT_BUILD_THEME.getTypeToBBData().get(OTHER);
+		}
+		return bbdList.get(rand.nextInt(bbdList.size()));
+	}
 }
