5a95cf9e4c84c7497b7e729cf5df0766cad7685c
ssnyderCoder <ogresean@yahoo.com>
2016-12-14 11:30:34 -0600

    Starting to program the Build Plan Templates

---
+++ src/main/java/com/seanModTest/templates/BuildModifiers.java
@@ -0,0 +1,25 @@
+package com.seanModTest.templates;
+
+public class BuildModifiers {
+
+	/* Build can be rotated along the y axis*/
+	public static final String CAN_ROTATE = "can_rotate";
+	/* Build can be stretched along the x axis*/
+	public static final String CAN_STRETCH_X = "can_stretch_x";
+	/* Build can be repeated along the y axis */
+	public static final String CAN_STRETCH_Y = "can_stretch_y";
+	/* Build can be repeated along the z axis */
+	public static final String CAN_STRETCH_Z = "can_stretch_z";
+	/* Build can be repeated along the x axis */
+	public static final String CAN_REPEAT_X = "can_repeat_x";
+	/* Build can be repeated along the y axis */
+	public static final String CAN_REPEAT_Y = "can_repeat_y";
+	/* Build can be repeated along the z axis */
+	public static final String CAN_REPEAT_Z = "can_repeat_z";
+	
+	
+	public BuildModifiers() {
+	}
+
+	//later in development, add some applyX() methods that can be called later
+}
--- src/main/java/com/seanModTest/templates/BuildPlanTemplate.java
+++ src/main/java/com/seanModTest/templates/BuildPlanTemplate.java
@@ -1,11 +1,52 @@
 package com.seanModTest.templates;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.sean.utility.TaggedDataProvider;
 
 public class BuildPlanTemplate {
 
-	public BuildPlanTemplate() {
-		// TODO Auto-generated constructor stub
+	private final Set<String> tags = new HashSet<String>();
+	private final Set<String> modifiers = new HashSet<String>(); // allow a template to be rotated, stretched, repeated, etc
+	private final String name;
+	private final int xSize;
+	private final int ySize;
+	private final int zSize;
+	
+	/*add BuildCube class that can be 1 of 2 things:
+	 * > Abstract: Has measurements(xyz), placement location(xyz), and tags
+	 * >>eg. 1x5x5 Wall placed at 0,0,0 in template with "wall, !hasWindows" tags
+	 * > Concrete: Has placement location, a 3d array of IDs, and ID identifiers
+	 * >>eg. (2x3x2) Wall placed at 0,0,0 with idArray[[0,1][0,1][0,1]][[1,0][1,0][1,0]]
+	 * >> with IDs: id0=theme.primary, id1=theme.secondary 
+	*/
+	
+	public BuildPlanTemplate(String uniqueName, int sizeX, int sizeY, int sizeZ) {
+		name = uniqueName;
+		xSize = sizeX;
+		ySize = sizeY;
+		zSize = sizeZ;
+	}
+	
+	public boolean addTag(String tag){
+		return tags.add(tag);
+	}
+	
+	public boolean addModifier(String modifier){
+		return modifiers.add(modifier);
+	}
+	
+	public boolean hasTag(String tag){
+		return tags.contains(tag);
+	}
+	
+	public boolean hasModifier(String modifier){
+		return modifiers.contains(modifier);
+	}
+	
+	public String getName(){
+		return name;
 	}
 	
 	public BuildPlan generateBuildPlan(TaggedDataProvider<BuildPlanTemplate> dataProvider){
